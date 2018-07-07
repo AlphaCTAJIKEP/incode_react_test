fb0002ac6b3da2328ddbed84d687e50a8ca4f69d
AlphaCTAJIKEP <v-gladko@mail.ua>
2018-07-07 18:13:24 +0300

    Создал reducer, роут элементы, контейнер {Layout}

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore,applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import {Route,Router,browserHistory} from 'react-router'
+import {syncHistoryWithStore} from 'react-router-redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import reducers from 'reducers'
+import Layout from 'containers/Layout'
+import Home from 'components/Home'
+
+import './index.css'
+
+const store = createStore(reducers,composeWithDevTools(applyMiddleware(thunk)))
+
+const history = syncHistoryWithStore(browserHistory,store)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <Route component={Layout}>
+        <Route path="/" component={Home}/>
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+)
